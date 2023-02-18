908be1332ddac8e448427841ef7a6f875993f627
ujjwal1309 <ujjwalsharma1309@gmail.com>
2023-02-18 12:19:42 +0530

    docs updated

--- evaluation/index.js
+++ evaluation/index.js
@@ -1,13 +1,37 @@
 const express = require("express");
 const { connection } = require("./config/db");
 const { booksRouter } = require("./routes/books.routes");
-const {validator}=require("./middlewares/validator");
+const { validator } = require("./middlewares/validator");
+const swaggerJSdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
-app.use(express.json())
-app.use("/books",validator);
+app.use(express.json());
+app.use(cors());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Learning Swagger",
+      version: "1.0.0",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000",
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const swaggerSpec = swaggerJSdoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+app.use("/books", validator);
 app.use("/books", booksRouter);
 
 app.listen(process.env.port, async () => {
--- evaluation/routes/books.routes.js
+++ evaluation/routes/books.routes.js
@@ -4,6 +4,51 @@ const fs = require("fs");
 
 const booksRouter = express.Router();
 
+/**
+ * @swagger
+ * components:
+ *    schemas:
+ *      Book:
+ *        type: object
+ *        properties:
+ *          id:
+ *            type: string
+ *            description: The auto-generated id of the user
+ *          name:
+ *            type: string
+ *            description: The user name
+ *          email:
+ *            type: string
+ *            description: The user email
+ *          age:
+ *            type: integer
+ *            description: Age of the user
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Books
+ *  description: All the API routes related to books
+ */
+
+/**
+ * @swagger
+ * /books:
+ *  get:
+ *      summary: This will get all the books data from the database
+ *      tags: [Books]
+ *      responses:
+ *          200:
+ *              description: The list of all the users
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: array
+ *                          item:
+ *                              $ref: "#/components/schemas/Book"
+ */
+
 booksRouter.get("/", async (req, res) => {
   const query = req.query;
   try {
@@ -24,6 +69,29 @@ booksRouter.get("/", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /books/add:
+ *  post:
+ *      summary: To add the details of the new book
+ *      tags: [Books]
+ *      requestedBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#components/schemas/Book"
+ *      responses:
+ *        200:
+ *          description: The user was successfully registered
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: "#components/schemas/Book"
+ *        500:
+ *          description: Some server error
+ */
+
 booksRouter.post("/add", async (req, res) => {
   try {
     const book = new BookModel(req.body);
@@ -33,6 +101,38 @@ booksRouter.post("/add", async (req, res) => {
     res.send({ msg: error, err: error.message });
   }
 });
+
+/**
+ * @swagger
+ * /books/update/{id}:
+ *  patch:
+ *    summary: It will update the user details
+ *    tags: [Books]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: "#/components/schemas/Book"
+ *    responses:
+ *      200:
+ *        description: The user details has been updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Book"
+ *      404:
+ *        description: The user was not found
+ *      500:
+ *        description: Some error happened
+ */
 
 booksRouter.patch("/update/:id", async (req, res) => {
   const id = req.params.id;
@@ -47,6 +147,32 @@ booksRouter.patch("/update/:id", async (req, res) => {
     res.send({ msg: error, err: error.message });
   }
 });
+
+/**
+ * @swagger
+ * /books/delete/{id}:
+ *  delete:
+ *    summary: It will delete the user details
+ *    tags: [Books]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The book id
+ *    responses:
+ *      200:
+ *        description: The book has been deleted
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: "#/components/schemas/Book"
+ *      404:
+ *        description: The user was not found
+ *      500:
+ *        description: Some error happened
+ */
 
 booksRouter.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
